fed2da8405712209f34e6961a20e8b63ea637e13
“Mrudul <mrudul.addipalli@coindcx.com>
2022-10-10 22:54:32 +0530

    code optimised

--- Week 3/101. Symmetric Tree.dart
+++ Week 3/101. Symmetric Tree.dart
@@ -2,22 +2,19 @@ import '../helper_data_structures.dart/binary_tree.dart';
 
 class Solution {
   bool isSymmetric(TreeNode? root) {
-      if(root == null) return true;
-      return isMirror(root.left,root.right);    
+    if (root == null) return true;
+    return isMirror(root.left, root.right);
   }
-  
-  bool isMirror(TreeNode? left,TreeNode? right){
+
+  bool isMirror(TreeNode? left, TreeNode? right) {
     if (left == null && right == null) return true;
-    if(left !=null && right!=null) {
-      // main logic - center should be equal
-      return 
-        // main logic - center should be equal
-        (left.val == right.val) && 
+    if (left == null || right == null) return false;
+
+    // main logic - center should be equal
+    return (left.val == right.val) &&
         // left should be mirror of right
-        isMirror(left.left,right.right) && 
+        isMirror(left.left, right.right) &&
         // right should be mirror of left
-        isMirror(left.right, right.left); 
-    };
-    return false;
+        isMirror(left.right, right.left);
   }
 }
