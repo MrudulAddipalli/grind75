7846c0868942e7797b46a545a3db13f55d731320
“Mrudul <mrudul.addipalli@coindcx.com>
2022-10-28 18:01:20 +0530

    102. Binary Tree Level Order Traversal added

---
+++ Week 4/102. Binary Tree Level Order Traversal.dart
@@ -0,0 +1,42 @@
+
+// Solution - https://www.youtube.com/watch?v=XZnWETlZZ14
+// Solution - https://www.youtube.com/watch?v=6ZnyEApgFYg
+
+import '../helper_data_structures.dart/binary_tree.dart';
+
+class Solution {
+  List<List<int>> levelOrder(TreeNode? root) {
+    List<List<int>> res = [];
+    if (root == null) return res;
+
+    // BFS Approach,
+    // DFS has in pre and post orders
+    List<TreeNode> queue = [];
+
+    // adding root as node for first level
+    queue.add(root);
+
+    while (queue.isNotEmpty) {
+      // this will give no. of nodes present in current level
+      int noOfNodesInQueueAsPerCurrentLevel = queue.length;
+
+      List<int> nodesInCurrentLevel = [];
+
+      for (int i = 0; i < noOfNodesInQueueAsPerCurrentLevel; i++) {
+        
+        // queue is FIFO, so we need to remove first element 
+        TreeNode currentNode = queue.removeAt(0);
+        nodesInCurrentLevel.add(currentNode.val);
+
+        // adding next level nodes of currentNode
+        if (currentNode.left != null) queue.add(currentNode.left!);
+        if (currentNode.right != null) queue.add(currentNode.right!);
+      }
+
+      // adding current level nodes to result array
+      res.add(nodesInCurrentLevel);
+    }
+
+    return res;
+  }
+}
