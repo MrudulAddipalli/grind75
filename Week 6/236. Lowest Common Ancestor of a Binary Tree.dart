df75a998a12347b7d7c02d2ce9c97fbbb7ee62c7
“Mrudul <mrudul.addipalli@coindcx.com>
2022-11-13 14:37:01 +0530

    236. Lowest Common Ancestor of a Binary Tree added

---
+++ Week 6/236. Lowest Common Ancestor of a Binary Tree.dart
@@ -0,0 +1,23 @@
+// Solution - https://www.youtube.com/watch?v=VG5w_VVAgH4
+
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+//         if(root==NULL)return NULL;
+        
+//         if(root==p || root==q)return root;  //if current node is same as 'p' OR 'q'.
+        
+//         TreeNode* left= lowestCommonAncestor(root->left,p,q);
+//         TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        
+//         if(left!=NULL && right!=NULL)
+//             return root;// this is the LCA node of p and q
+
+
+//         if(left!=NULL)
+//             return left;
+//         else // 
+//             return right;
+//     }
+// };
