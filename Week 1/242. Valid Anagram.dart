bfc734ed3782026f996997c036ad6a662cf2cba5
Mrudul Addipalli <mruduladdipalli@gmail.com>
2022-12-01 00:43:02 +0530

    comment added for 242. Valid Anagram.dart

--- Week 1/242. Valid Anagram.dart
+++ Week 1/242. Valid Anagram.dart
@@ -1,11 +1,23 @@
 // Runtime: 622 ms, faster than 9.68% of Dart online submissions for Valid Anagram.
 // Memory Usage: 141.3 MB, less than 97.85% of Dart online submissions for Valid Anagram.
 
+// Solution - https://www.youtube.com/watch?v=9UtInBqnCgA
+
+// Time - O(n)
+// Space - O(n) - single map
+
+
+// For Constant Space Complexity O(1)
+// Sort both array and check if they are equal
+// but here Time Complexity becomes - O(n logn) 
 
 class Solution {
   bool isAnagram(String s, String t) {
     int sLen = s.length, lLen = t.length, i;
+
+    // Base Case
     if (sLen != lLen) return false;
+
     Map<String, int> char = {};
     for (i = 0; i < sLen; i++) {
       if (char[s[i]] == null) {
