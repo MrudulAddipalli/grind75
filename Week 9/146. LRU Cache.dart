c0a7594bedb1224c5c2dbea39656e28a4f0b1750
Mrudul Addipalli <mruduladdipalli@gmail.com>
2022-12-10 22:42:33 +0530

    146. LRU Cache.dart added

--- Week 9/146. LRU Cache.dart
+++ Week 9/146. LRU Cache.dart
@@ -1,17 +1,20 @@
 // Explaination - https://www.youtube.com/watch?v=xDEuM5qa0zg
 // Solution - https://www.youtube.com/watch?v=7ABFKPK2hD4&t=855s
-// Solution video considers LRU as left, we consider as right,
+// Solution video considers LRU as head, we consider as tail,
 
-// Note - below code, considers right as LRU, and left as Most Recently Used
+// Note - below code, considers tail as LRU, and head as Most Recently Used
 
 // Algorithm for O(1) Time Complexity
 // Initial Head And Tail will always be poining to Node with 0,0
-// while put, if capacity is Not Full, then new element will added right after head
-// while put, if capacity is Full, then node before tail will be deleted and new element will added right after head
-// while put, if element already exist, then remove node before tail and right after head
+// while put, if capacity is Not Full, then new element will added tail after head
+// while put, if capacity is Full, then node before tail will be deleted and new element will added tail after head
+// while put, if element already exist, then remove node before tail and tail after head
 // while get, if element is present then remove from DLL and put after head
 // while get, if element is not present in hashmap then return -1
 // Note: all delete and add in DDL will updated in HashMap everytime
+
+// Time - 84.62%
+// Memory - 100%
 
 class Node {
   int key, value;
@@ -22,40 +25,40 @@ class Node {
 class LRUCache {
   int capacity = 0;
   Map<int, Node> cache = {};
-  Node? left, right;
+  Node head = new Node(0, 0);
+  Node tail = new Node(0, 0);
 
   LRUCache(int capacity) {
     this.capacity = capacity;
-    left = right = Node(0, 0);
-    left?.next = right;
-    right?.prev = left;
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
   }
 
   void remove(Node? node) {
-    // we don't right node here, because while adding node links are updated correctly
-    // and new node is added from left node, hence LRU will always be on right
-    // and current node always get linked to right node, which will be linked to last right node [ 0,0 ]  
-    Node? prev = node?.prev;
-    Node? next = node?.next;
-    prev?.next = next;
-    node?.prev = prev;
+    // remove from map
+    this.cache.remove(node?.key);
+    node?.prev?.next = node.next;
+    node?.next?.prev = node.prev;
   }
 
   void insert(Node? node) {
-    Node? prev = left?.prev;
-    Node? next = left;
-    prev?.next = next?.prev = node;
-    node?.next = next;
-    node?.prev = prev;
+    if (node != null) {
+      this.cache[node.key] = node;
+      Node? headNext = this.head.next;
+      this.head.next = node;
+      node.next = headNext;
+      headNext?.prev = node;
+      node.prev = head;
+    }
   }
 
   int get(int key) {
-    if (cache.containsKey(key)) {
-      Node node = cache[key]!;
-      // remove from right
-      remove(node);
-      // inser from left
-      insert(node);
+    if (this.cache.containsKey(key)) {
+      Node node = this.cache[key]!;
+      // remove from tail
+      this.remove(node);
+      // insert from head
+      this.insert(node);
       return node.value;
     }
     return -1;
@@ -63,23 +66,17 @@ class LRUCache {
 
   void put(int key, int value) {
     // remove already existing node
-    if (cache.containsKey(key)) {
-      Node node = cache[key]!;
-      // remove from right
-      remove(node);
+    if (this.cache.containsKey(key)) {
+      // remove from tail
+      this.remove(this.cache[key]);
     }
 
-    // update cache and new new node in DLL
-    Node newNode = Node(key, value);
-    cache[key] = newNode;
-    // insert from left
-    insert(newNode);
-
-    // if capacity is exceeding, remove LRU , which node before tail node
-    if (cache.length > capacity) {
-      Node? lru = right?.prev;
-      remove(lru);
-      cache.remove(lru?.key);
+    // if capacity is equal [ means cannot add new node until we have space ],
+    // remove LRU , which node before tail node
+    if (this.cache.keys.length == capacity) {
+      this.remove(this.tail.prev);
     }
+    // insert from head
+    this.insert(new Node(key, value));
   }
 }
