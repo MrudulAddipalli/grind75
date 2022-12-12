ac91533c2545fa9613ae34e20d4de438088a2886
Mrudul Addipalli <mruduladdipalli@gmail.com>
2022-12-13 01:12:14 +0530

    1. Two Sum.dart updated solution

--- Week 1/1. Two Sum.dart
+++ Week 1/1. Two Sum.dart
@@ -1,13 +1,29 @@
+// Solution  - https://www.youtube.com/watch?v=KLlXCFG5TnA
+
+// Time - O(n)
+// Space - O(n) - for map
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-      for(int i=0;i<nums.length;i++){
-          int k = (i+1 == nums.length) ? nums.length :  i+1;
-           for(int j=k;j<nums.length;j++){
-             if(nums[i]+nums[j] == target){
-              return [i,j];
-            }
-         }
+    Map<int, int> map = {}; // value : index
+    for (int i = 0; i < nums.length; i++) {
+      int diff = target - nums[i];
+      if (map.containsKey(diff)) return [map[diff]!, i];
+      map[nums[i]] = i;
+    }
+    return [];
+  }
+}
+
+class Solution2 {
+  List<int> twoSum(List<int> nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      int k = (i + 1 == nums.length) ? nums.length : i + 1;
+      for (int j = k; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return [i, j];
+        }
       }
-     return [];
+    }
+    return [];
   }
 }
