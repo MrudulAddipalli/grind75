4c5999d3dc849b64c9f0563c37722ed86978d7a9
Mrudul Addipalli <mruduladdipalli@gmail.com>
2022-12-14 01:16:29 +0530

    198. House Robber.dart working solution added

--- Week 9/198. House Robber.dart
+++ Week 9/198. House Robber.dart
@@ -1,13 +1,30 @@
+// Solution - https://www.youtube.com/watch?v=73r3KWiEvyk
+class Solution {
+  int rob(List<int> nums) {
+    int rob1 = 0, rob2 = 0;
+    for (int i = 0; i < nums.length; i++) {
+      print(
+          "\n\ncurrent \nnumber - ${nums[i]}\nrob1 - $rob1, \nrob2 - $rob2, \nmax(${nums[i]} + $rob1, $rob2) ");
+      final temp = max(nums[i] + rob1, rob2);
+      rob1 = rob2;
+      rob2 = temp;
+    }
+    print("\n\nfinal \nrob1 - $rob1, \nrob2 - $rob2");
+
+    return rob2;
+  }
+
+  int max(int a, int b) => a > b ? a : b;
+}
 
 
 // not working for [2,1,1,2], since we are getting 4 from 1st and 4th index, and our logic only works for 0-2-4 or 1-3-5 combinations
-class Solution {
+class Solution2 {
   int rob(List<int> nums) {
     int biggestRobbedAmount = 0;
     int currentAmount = 0;
     robFrom({required int startIndex}) {
       for (int i = startIndex; i < nums.length; i = i + 2) {
-        print("${nums[i]} @ index $i");
         currentAmount += nums[i];
         biggestRobbedAmount = max(biggestRobbedAmount ,currentAmount);
       }
