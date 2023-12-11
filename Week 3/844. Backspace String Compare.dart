28f834d63d340d005b416ae752078b778b3f9e92
Mrudul Addipalli <mruduladdipalli@gmail.com>
2023-12-11 10:08:18 +0530

    code comment added

--- Week 3/844. Backspace String Compare.dart
+++ Week 3/844. Backspace String Compare.dart
@@ -1,3 +1,18 @@
+// void main() async {
+//   print(remove("ab#c") == remove("ac#c"));
+// }
+
+// String remove(String word){
+//   if(word.isEmpty) return word;
+//   // find #
+//   int i=0;
+//   for(i=0; i< word.length;i++){
+//     if(word[i] == "#") break;
+//   }
+//   if(i == word.length) return word;
+//   word = word.substring(0,i-1) + word.substring(i+1,word.length);
+//   return remove(word);
+// }
 
 
 // Runtime: 448 ms, faster than 75.00% of Dart online submissions for Backspace String Compare.
